d7b5f2be9e78b0c1851c1d2b4818a15585003d0a
Methu-v1 <methubot@gmail.com>
2025-05-23 21:15:09 +0530

    Update auto=reply+sticker+voice.js

--- plugins/auto=reply+sticker+voice.js
+++ plugins/auto=reply+sticker+voice.js
@@ -8,7 +8,7 @@ cmd({
   on: "body"
 },    
 async (conn, mek, m, { from, body, isOwner }) => {
-    const data = await fetchJson(`your json link`)
+    const data = await fetchJson(`https://raw.githubusercontent.com/Methu-v1/sudu/refs/heads/main/my_data/autoreply.json`)
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
             if (config.AUTO_REPLY === 'true') {
